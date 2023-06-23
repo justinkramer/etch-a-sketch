be80a74bde62aa494c05f216928b8376a424d616
Justin Kramer <jkramer@dmsgroup.com>
2023-06-23 03:55:41 -0400

    add etch-a-sketch functionality
    - add background color change on hover
    - add clear button
    - add resize button

--- scripts.js
+++ scripts.js
@@ -7,4 +7,43 @@ let makeGrid = (rows, cols) => {
     }
 }
 
-makeGrid(16, 16);
+let calculateFlexBasis = (rows) => {
+    let flexPercentage = (1/rows) * 100;
+    return flexPercentage;
+}
+
+let clear = () => {
+    let grid = document.getElementById('grid');
+    let boxes = document.getElementsByClassName('grid-item');
+    let sqrt = Math.sqrt(boxes.length);
+    grid.innerHTML = '';
+    makeGrid(sqrt, sqrt);
+    let newBoxes = document.getElementsByClassName('grid-item');
+    let flexPercentage = calculateFlexBasis(sqrt);
+    Array.from(newBoxes).forEach(box => {
+        box.style.flexBasis = `${flexPercentage}%`;
+    });
+}
+
+let resize = () => {
+    let grid = document.getElementById('grid');
+    grid.innerHTML = '';
+    let rows = prompt('How many rows?');
+    makeGrid(rows, rows);
+    let boxes = document.getElementsByClassName('grid-item');
+    let flexPercentage = calculateFlexBasis(rows);
+    Array.from(boxes).forEach(box => {
+        box.style.flexBasis = `${flexPercentage}%`;
+    });
+}
+
+makeGrid(100, 100);
+
+document.addEventListener('mouseover', function(e) {
+    if (e.target.className === 'grid-item') {
+        e.target.style.backgroundColor = 'black';
+    }
+});
+
+document.getElementById('clear').addEventListener('click', clear);
+document.getElementById('resize').addEventListener('click', resize);
